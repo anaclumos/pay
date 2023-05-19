dbabc922d9a85ff515b52eebb6551f6166e1edf2
Sunghyun Cho <mail@chosunghyun.com>
2023-05-18 23:37:15 -0700

    Refactor success page layout

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -98,19 +98,17 @@ export default function SuccessPage({ payment }: Props) {
 
           <div className='bg-white py-12 md:py-24'>
             <div className='mx-auto max-w-lg px-4 lg:px-8'>
-              <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <h1 className='text-2xl font-medium tracking-tight text-gray-900'>Success</h1>
-                <h2 className='text-2xl font-medium tracking-tight text-gray-900'>Order Name</h2>
-                <p className='text-2xl font-medium tracking-tight text-gray-900'>{payment.orderName}</p>
-                <h2 className='text-2xl font-medium tracking-tight text-gray-900'>Approved At</h2>
-                <p className='text-2xl font-medium tracking-tight text-gray-900'>{format(new Date(payment.approvedAt), 'yyyy-MM-dd HH:mm:ss')}</p>
-                <h2 className='text-2xl font-medium tracking-tight text-gray-900'>Receipt</h2>
-                <Link href={payment.receipt.url}>Receipt</Link>
-                <h2 className='text-2xl font-medium tracking-tight text-gray-900'>Total Amount</h2>
-                <p className='text-2xl font-medium tracking-tight text-gray-900'>{payment.totalAmount}</p>
-                <h2 className='text-2xl font-medium tracking-tight text-gray-900'>Method</h2>
-                <p className='text-2xl font-medium tracking-tight text-gray-900'>{payment.method}</p>
-              </main>
+              <h1 className='text-2xl font-medium tracking-tight text-gray-900'>Success</h1>
+              <h2 className='text-xl font-medium tracking-tight text-gray-900'>Order Name</h2>
+              <p className='text-md font-medium tracking-tight text-gray-900'>{payment.orderName}</p>
+              <h2 className='text-xl font-medium tracking-tight text-gray-900'>Approved At</h2>
+              <p className='text-md font-medium tracking-tight text-gray-900'>{format(new Date(payment.approvedAt), 'yyyy-MM-dd HH:mm:ss')}</p>
+              <h2 className='text-xl font-medium tracking-tight text-gray-900'>Receipt</h2>
+              <Link href={payment.receipt.url}>Receipt</Link>
+              <h2 className='text-xl font-medium tracking-tight text-gray-900'>Total Amount</h2>
+              <p className='text-md font-medium tracking-tight text-gray-900'>{payment.totalAmount}</p>
+              <h2 className='text-xl font-medium tracking-tight text-gray-900'>Method</h2>
+              <p className='text-md font-medium tracking-tight text-gray-900'>{payment.method}</p>
             </div>
           </div>
         </div>
