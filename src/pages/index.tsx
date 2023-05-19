49ab0e3a23e5dcf74c64ba97afd9e283f338b020
Sunghyun Cho <mail@chosunghyun.com>
2023-05-18 22:54:56 -0700

    bootstrap files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,113 +5,166 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='grid min-h-screen place-items-center'>
+      <div className={`flex flex-col items-center justify-between w-full min-h-screen p-4 ${inter.className}`}>
+        <h1 className='sr-only'>Checkout</h1>
+        <div className='m-auto grid grid-cols-1 md:grid-cols-2 w-full'>
+          <div className='bg-gray-100 py-24 md:py-24 rounded-3xl'>
+            <div className='mx-auto max-w-lg space-y-8 px-4 lg:px-8'>
+              <div className='flex items-center gap-4'>
+                <span className='h-10 w-10 rounded-full bg-blue-700'></span>
+                <h2 className='font-medium text-gray-900'>pay.cho.sh</h2>
+              </div>
+              <div>
+                <p className='text-2xl font-medium tracking-tight text-gray-900'>$0.01</p>
+                <p className='mt-1 text-sm text-gray-600'>I graciously purchase</p>
+              </div>
+
+              <div>
+                <div className='flow-root'>
+                  <ul className='-my-4 divide-y divide-gray-100'>
+                    <li className='flex items-center gap-4 py-4'>
+                      <Image src='https://github.com/anaclumos.png' alt='' width={64} height={64} className='h-16 w-16 rounded object-cover' />
+                      <div>
+                        <h3 className='text-sm text-gray-900'>Autograph of Sunghyun Cho</h3>
+
+                        <dl className='mt-0.5 space-y-px text-[10px] text-gray-600'>
+                          <div>
+                            <dt className='inline'>Sent via: </dt>
+                            <dd className='inline'>Email</dd>
+                          </div>
+                        </dl>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='bg-white py-12 md:py-24'>
+            <div className='mx-auto max-w-lg px-4 lg:px-8'>
+              <form className='grid grid-cols-6 gap-4'>
+                <div className='col-span-3'>
+                  <label htmlFor='FirstName' className='block text-xs font-medium text-gray-700'>
+                    First Name
+                  </label>
+
+                  <input type='text' id='FirstName' className='mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm' />
+                </div>
+
+                <div className='col-span-3'>
+                  <label htmlFor='LastName' className='block text-xs font-medium text-gray-700'>
+                    Last Name
+                  </label>
+                  <input type='text' id='LastName' className='mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm' />
+                </div>
+
+                <div className='col-span-6'>
+                  <label htmlFor='Email' className='block text-xs font-medium text-gray-700'>
+                    Email
+                  </label>
+
+                  <input type='email' id='Email' className='mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm' />
+                </div>
+
+                <div className='col-span-6'>
+                  <label htmlFor='Phone' className='block text-xs font-medium text-gray-700'>
+                    Phone
+                  </label>
+
+                  <input type='tel' id='Phone' className='mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm' />
+                </div>
+
+                <fieldset className='col-span-6'>
+                  <legend className='block text-sm font-medium text-gray-700'>Card Details</legend>
+
+                  <div className='mt-1 -space-y-px rounded-md bg-white shadow-sm'>
+                    <div>
+                      <label htmlFor='CardNumber' className='sr-only'>
+                        {' '}
+                        Card Number{' '}
+                      </label>
+                      <input
+                        type='text'
+                        id='CardNumber'
+                        placeholder='Card Number'
+                        className='relative mt-1 w-full rounded-t-md border-gray-200 focus:z-10 sm:text-sm'
+                      />
+                    </div>
+
+                    <div className='flex'>
+                      <div className='flex-1'>
+                        <label htmlFor='CardExpiry' className='sr-only'>
+                          {' '}
+                          Card Expiry{' '}
+                        </label>
+
+                        <input
+                          type='text'
+                          id='CardExpiry'
+                          placeholder='Expiry Date'
+                          className='relative w-full rounded-es-md border-gray-200 focus:z-10 sm:text-sm'
+                        />
+                      </div>
+
+                      <div className='-ms-px flex-1'>
+                        <label htmlFor='CardCVC' className='sr-only'>
+                          {' '}
+                          Card CVC{' '}
+                        </label>
+
+                        <input
+                          type='text'
+                          id='CardCVC'
+                          placeholder='CVC'
+                          className='relative w-full rounded-ee-md border-gray-200 focus:z-10 sm:text-sm'
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </fieldset>
+
+                <fieldset className='col-span-6'>
+                  <legend className='block text-sm font-medium text-gray-700'>Billing Address</legend>
+
+                  <div className='mt-1 -space-y-px rounded-md bg-white shadow-sm'>
+                    <div>
+                      <label htmlFor='Country' className='sr-only'>
+                        Country
+                      </label>
+
+                      <input
+                        type='text'
+                        id='Country'
+                        placeholder='Country'
+                        className='relative mt-1 w-full rounded-t-md border-gray-200 focus:z-10 sm:text-sm'
+                      />
+                    </div>
+
+                    <div>
+                      <label className='sr-only' htmlFor='PostalCode'>
+                        {' '}
+                        ZIP/Post Code{' '}
+                      </label>
+
+                      <input
+                        type='text'
+                        id='PostalCode'
+                        placeholder='ZIP/Post Code'
+                        className='relative w-full rounded-b-md border-gray-200 focus:z-10 sm:text-sm'
+                      />
+                    </div>
+                  </div>
+                </fieldset>
+
+                <div className='col-span-6'>
+                  <button className='block w-full rounded-md bg-black p-2.5 text-sm text-white transition hover:shadow-lg'>Pay Now</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
